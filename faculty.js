a318ab5e4890208444ef9f710660fdd6c43dc6c4
tjamesw123 <94910672+tjamesw123@users.noreply.github.com>
2026-01-12 21:50:58 -0500

    Removed test button

--- faculty.js
+++ faculty.js
@@ -1,27 +1,27 @@
 //<script id="__NEXT_DATA__" type="application/json">...</script>
 
 
-var URLtoOriginDepartment = new Map([
-    ["https://www.stevens.edu/school-engineering-science/faculty", "Engineering and Science"],
-    ["https://www.stevens.edu/hass/hass-faculty", "Humanities, Arts and Social Sciences"],
-    ["https://www.stevens.edu/school-business/faculty", "Business"]
-]);
+// var URLtoOriginDepartment = new Map([
+//     ["https://www.stevens.edu/school-engineering-science/faculty", "Engineering and Science"],
+//     ["https://www.stevens.edu/hass/hass-faculty", "Humanities, Arts and Social Sciences"],
+//     ["https://www.stevens.edu/school-business/faculty", "Business"]
+// ]);
 
-const facultyButton = document.getElementById('facultyButton');
-facultyButton.addEventListener('click', function() {
-        console.log("Faculty Button Pressed");
-        var websitesToSearch = ["https://www.stevens.edu/school-engineering-science/faculty", "https://www.stevens.edu/hass/hass-faculty", "https://www.stevens.edu/school-business/faculty"];
-        console.log(websitesToSearch[0]);
-        var htmlText2 = fetchWebsiteHTML(websitesToSearch[0]);
+// const facultyButton = document.getElementById('facultyButton');
+// facultyButton.addEventListener('click', function() {
+//         console.log("Faculty Button Pressed");
+//         var websitesToSearch = ["https://www.stevens.edu/school-engineering-science/faculty", "https://www.stevens.edu/hass/hass-faculty", "https://www.stevens.edu/school-business/faculty"];
+//         console.log(websitesToSearch[0]);
+//         var htmlText2 = fetchWebsiteHTML(websitesToSearch[0]);
         
-});
+// });
 
-async function fetchWebsiteHTML(url) {
-    var a = await fetch(url); 
-    var htmlText = await a.text();
-    console.log(htmlText);
-    var groupsRegex = /(<script id="__NEXT_DATA__" type="application\/json">)(.+)(<\/script>)/.exec(htmlText);
-    console.log(groupsRegex);
-    console.log("HEYYY");
-    return htmlText;
-}
+// async function fetchWebsiteHTML(url) {
+//     var a = await fetch(url); 
+//     var htmlText = await a.text();
+//     console.log(htmlText);
+//     var groupsRegex = /(<script id="__NEXT_DATA__" type="application\/json">)(.+)(<\/script>)/.exec(htmlText);
+//     console.log(groupsRegex);
+//     console.log("HEYYY");
+//     return htmlText;
+// }
